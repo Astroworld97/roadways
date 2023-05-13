85df3e1b45a600c80e5e81aa6dfb0fe7ca4a8384
Ian Emmanuel Gonzalez Alfonzo <iagonzal97@gmail.com>
2023-05-13 16:51:08 -0500

    finished implementation of minHeap

--- src/MinHeap.java
+++ src/MinHeap.java
@@ -23,8 +23,116 @@ public class MinHeap {
         current_heap_size = 0;
     }
 
-    private void swap(){
+    //node swap
+    private void swap(int[] arr, int a, int b){
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
 
+    //returns the parent's index for the given node index
+    private int parent(int key){
+        return (key-1)/2;
+    }
+
+    //returns the left index for the given node index
+    private int left(int key){
+        return 2*key + 1;
+    }
+
+    //returns the right index for the given node index
+    private int right(int key){
+        return 2*key + 2;
+    }
+
+    // Inserts a new key
+    public boolean insertKey(int key){
+        if (current_heap_size==capacity){
+            return false;
+        }
+        int i = current_heap_size;
+        heapArray[i] = key;
+        current_heap_size++;
+
+        //fix min heap property if violated
+        while(i!=0 && heapArray[i] < heapArray[parent(i)]){
+            swap(heapArray, i, parent(i));
+            i = parent(i);
+        }
+        return true;
+    }
+
+    // Decreases value of given key to new_val.
+    public void decreaseKey(int key, int new_val){
+        heapArray[key] = new_val;
+
+        while(key!=0 && heapArray[key] < heapArray[parent(key)]){
+            swap(heapArray, key, parent(key));
+            key = parent(key);
+        }
+    }
+
+    //returns the key of the root of the minHeap
+    public int getMin(){
+        return heapArray[0];
+    }
+
+    public int extractMin(){
+        if(current_heap_size <= 0){
+            return Integer.MAX_VALUE;
+        }
+        if (current_heap_size==1){
+            current_heap_size--;
+            return heapArray[0];
+        }
+
+        //Store the minimum element and remove it from the heap
+        int root = heapArray[0];
+
+        heapArray[0] = heapArray[current_heap_size - 1];
+        current_heap_size--;
+        MinHeapify(0);
+
+        return root;
+    }
+
+    public void deleteKey(int key){
+        decreaseKey(key, Integer.MIN_VALUE);
+        extractMin();
+    }
+
+    private void MinHeapify(int key){
+        int l = left(key);
+        int r = right(key);
+
+        int smallest = key;
+        if(l < current_heap_size && heapArray[l] < heapArray[smallest]){
+            smallest = l;
+        }
+        if(r < current_heap_size && heapArray[r] < heapArray[smallest]){
+            smallest = r;
+        }
+        if (smallest != key){
+            swap(heapArray, key, smallest);
+            MinHeapify(smallest);
+        }
+    }
+
+    // Increases value of given key to new_val.
+    public void increaseKey(int key, int new_val){
+        heapArray[key] = new_val;
+        MinHeapify(key);
+    }
+
+    public void changeValueOnAKey(int key, int new_val){
+        if(heapArray[key] == new_val){
+            return;
+        }
+        if (heapArray[key] < new_val){
+            increaseKey(key, new_val);
+        }else{
+            decreaseKey(key, new_val);
+        }
     }
 
 }
